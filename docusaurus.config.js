4e880e6c7180b440b71b42da0db72465530396ab
dehjvg <hj@v-gijssel.nl>
2024-12-14 19:54:46 +0100

    link edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,8 +26,10 @@ const config = {
   projectName: 'docu', // Usually your repo name.
   deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'warn',
+
+
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
